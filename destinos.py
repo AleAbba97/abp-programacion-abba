dd0bba6f1c47f8c54384126c532526b24a79f8d8
AleAbba97 <abbaale97@gmail.com>
2025-05-29 18:53:28 -0300

    modifico destinos.py y agrego ventas.py

---
+++ destinos.py
@@ -0,0 +1,15 @@
+def agregar_destino():
+    print("""Nuevo Destino:
+    Ciudad:
+    Pais:
+    Costo base del viaje: 
+    """)
+
+def modificar_destino():
+    print(">>> Función: Modificando un destino existente...")
+
+def eliminar_destino():
+    print(">>> Función: Eliminando un destino...")
+
+def ver_destinos():
+    print(">>> Función: Mostrando la lista de destinos...")
---
+++ ventas.py
@@ -0,0 +1,7 @@
+def gestionar_ventas():
+    print(">>> Ingrese su numero de venta: ")
+    print (" Su venta se encuentra: (Activa/Desactiva)")
+
+def consultar_ventas():
+    print("Ingrese numero de venta:")
+    print("La venta pertenece al cuit: (numero de cuil) y es al destino (destino)")
